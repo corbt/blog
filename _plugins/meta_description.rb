376e64e15cd643940d87f37429430aec9bbfb636
Kyle Corbitt <kyle@corbt.com>
2014-05-09 20:39:29 -0600

    added meta description

--- _plugins/meta_description.rb
+++ _plugins/meta_description.rb
@@ -1,14 +1,16 @@
 require 'json'
+require 'sanitize'
 module Jekyll
   class MetaDescription < Liquid::Tag
     def render(context)
       if context['page'].has_key? 'description'
         @description = context['page']['description']
       else
-        @description = context['page']['content'].split('.').first+"."
+        @description = context['page']['excerpt']
       end
-
-      "<meta property='og:description' content=#{JSON.generate(@description,quirks_mode: true)}/>"
+      @description = JSON.generate(Sanitize.clean(@description),quirks_mode: true)
+      "<meta property='og:description' content=#{@description}/>"+
+      "<meta name='description' content=#{@description}/>"
     end
   end
 end
