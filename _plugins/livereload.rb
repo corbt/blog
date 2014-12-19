2c6ee9f42595231aca6de937e59bbeb55f4ba739
Kyle Corbitt <kylecorbitt@gmail.com>
2014-12-19 16:26:19 +0100

    prettier links and footnotes

--- _plugins/livereload.rb
+++ _plugins/livereload.rb
@@ -1,7 +1,7 @@
 module Jekyll
   class LiveReload < Liquid::Tag
     def render(context)
-      '<script src="http://localhost:35729/livereload.js"></script>' if ENV['JEKYLL_ENV'] != 'production'
+      '<script src="http://localhost:35729/livereload.js"></script>' unless ENV['JEKYLL_ENV'] == 'production'
     end
   end
 end
